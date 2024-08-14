1e9d2382a995a16040ca282993bac685d77d2bc6
Yibin Liu <liuyibin@oppo.com>
2024-08-14 16:54:22 +0800

    Format

--- hellokvm.c
+++ hellokvm.c
@@ -1,15 +1,12 @@
 #include <linux/kvm.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include "code.h"
 
 #define RAM_SIZE 128*1024*1024
-
-#include "code.h"
 
 int main(int argc, char **argv)
 {
@@ -20,9 +17,12 @@ int main(int argc, char **argv)
 	void *vmram;
 
 	kvmfd = open("/dev/kvm", O_RDWR);
+	printf("kvmfd: %d\n", kvmfd);
 	vmfd = ioctl(kvmfd, KVM_CREATE_VM, 0);
+	printf("vmfd: %d\n", vmfd);
 	vmram = mmap(NULL, RAM_SIZE, PROT_READ | PROT_WRITE,
 			MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	printf("vmram: 0x%lx\n", (long)vmram);
 	memcpy(vmram, guest_code, sizeof(guest_code));
 
 	mem.slot = 0;
@@ -31,34 +31,17 @@ int main(int argc, char **argv)
 	mem.memory_size = RAM_SIZE;
 	mem.userspace_addr = (__u64) vmram;
 
-	if (ioctl(vmfd, KVM_SET_USER_MEMORY_REGION, &mem) < 0) {
-		perror("error setting memory region: ");
-		return -1;
-	}
-
+	ioctl(vmfd, KVM_SET_USER_MEMORY_REGION, &mem);
 	vcpufd = ioctl(vmfd, KVM_CREATE_VCPU, 0);
-	if (vcpufd < 0) {
-		perror("error creating vcpu: ");
-		return -1;
-	}
-
+	printf("vcpufd: %d\n", vcpufd);
 	vcpu_size = ioctl(kvmfd, KVM_GET_VCPU_MMAP_SIZE, 0);
-	if (vcpu_size < 0) {
-		perror("error getting vCPU mmap size: ");
-		return -1;
-	}
-
-	vcpu = mmap(NULL, vcpu_size, PROT_READ | PROT_WRITE, MAP_SHARED, vcpufd, 0);
-	if (vcpu == MAP_FAILED) {
-		perror("error mapping vcpu: ");
-		return -1;
-	}
+	printf("vcpu_size: %d\n", vcpu_size);
+	vcpu = mmap(NULL, vcpu_size, PROT_READ | PROT_WRITE,
+			MAP_SHARED, vcpufd, 0);
+	printf("vcpu: 0x%lx\n", (long)vcpu);
 
 	while (1) {
-		if (ioctl(vcpufd, KVM_RUN, 0) < 0) {
-			perror("error running vCPU: ");
-			return -1;
-		}
+		ioctl(vcpufd, KVM_RUN, 0);
 
 		switch (vcpu->exit_reason) {
 		case KVM_EXIT_INTERNAL_ERROR:
